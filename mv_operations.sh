3796e72376e9f5eb12dae317adb86b47e34ddc06
YASVANTH KANNAN L <yasvanthkannan55@gmail.com>
2026-09-07 13:10:20 +0530

    UNIX PROGRAM 9 IU25IT118

--- mv_operations.sh
+++ mv_operations.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+case "$1" in
+
+move)
+    mv file1.txt documents/
+    ;;
+
+rename)
+    mv file1.txt file2.txt
+    ;;
+
+multiple)
+    mv file1.txt file2.txt documents/
+    ;;
+
+directory)
+    mv source_dir documents/
+    ;;
+
+esac
